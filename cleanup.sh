4d7ee2a534467e69c1c2488368da067fc16dd2d8
aodq <dtoadq@gmail.com>
2020-04-15 22:03:40 -0400

    adds more models

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,22 @@
 
 # mcguire
 pushd mcguire
+rm -r bedroom
 rm -r bmw
+rm -r breakfast-room
+rm -r chinese-dragon
+rm -r clouds
+rm -r conference-room
+rm -r cornell-box
+rm -r sponza-crytek
+
+
+git restore bedroom.tar.gz
+git restore bmw.tar.gz
+git restore breakfast-room.tar.gz
+git restore chinese-dragon.tar.gz
+git restore clouds.tar.gz
+git restore conference-room.tar.gz
+git restore cornell-box.tar.gz
+git restore sponza-crytek.tar.gz
 popd
--- extract.sh
+++ extract.sh
@@ -2,5 +2,22 @@
 
 # mcguire
 pushd mcguire
+tar -xzvf bedroom.tar.gz
 tar -xzvf bmw.tar.gz
+tar -xzvf breakfast-room.tar.gz
+tar -xzvf chinese-dragon.tar.gz
+tar -xzvf clouds.tar.gz
+tar -xzvf conference-room.tar.gz
+tar -xzvf cornell-box.tar.gz
+tar -xzvf sponza-crytek.tar.gz
+
+
+rm -r bedroom.tar.gz
+rm -r bmw.tar.gz
+rm -r breakfast-room.tar.gz
+rm -r chinese-dragon.tar.gz
+rm -r clouds.tar.gz
+rm -r conference-room.tar.gz
+rm -r cornell-box.tar.gz
+rm -r sponza-crytek.tar.gz
 popd
